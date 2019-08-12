dab5a866903725b149bb1b40b21d15e98c7eac1e
Mike <25lion52@gmail.com>
2019-08-12 15:15:56 +0300

    add core io

---
+++ RaspberryIO/LightAndTemperature/Channels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaspberryIO.LightAndTemperature
+{
+    public enum LightChannels
+    {
+        LivingRoom0,
+        LivingRoom1,
+        Bedroom0,
+        Bedroom1,
+        Kitchen0,
+        Kitchen1,
+        Balcony0,
+        Balcony1,
+        Bathroom0,
+        Bathroom1,
+        Corridor0,
+        Corridor1
+    }
+}
